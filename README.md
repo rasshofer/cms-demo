49649dc99b3d9855b76aed3242c9b0672808af7f
Thomas Rasshofer <hello@thomasrasshofer.com>
2017-04-08 13:28:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# CMS Demo
+
+> A very simple demo site built using the »cms« package
+
+This demo site is built using the [cms](https://github.com/rasshofer/cms) package and is intended to demonstrate the following »unusualnesses«.
+
+- Usage in combination with other build steps (using [Gulp](https://www.npmjs.com/package/gulp))
+- Different templates (`home`, `overview`, `page`, `report`)
+- [Pug](http://npmjs.com/package/pug) as template engine (using [Consolidate](https://www.npmjs.com/package/consolidate))
+- Custom `markdown` addon (using [Marked](http://npmjs.com/package/marked))
+- Custom YouTube shortcode
+- Custom paths for templates, content, and output
+
+## Usage
+
+```
+npm install
+```
+
+```
+npm run build
+```
+
+## License
+
+Copyright (c) 2017 [Thomas Rasshofer](http://thomasrasshofer.com/)  
+Licensed under the MIT license.
+
+See LICENSE for more info.
+
+The following photos from [Unsplash](https://unsplash.com/) have been used as exemplary content.
+
+- [Canada 1](https://unsplash.com/photos/UGZcx5V_HBE)
+- [Canada 2](https://unsplash.com/photos/RlThtfyCd5k)
+- [Canada 3](https://unsplash.com/photos/O-zozg13yuk)
+- [Canada 4](https://unsplash.com/photos/pCyF1HIMovI)
+- [New York 1](https://unsplash.com/photos/zzAog9QvVFk)
+- [New York 2](https://unsplash.com/photos/vJLP9PY5BCE)
+- [New York 3](https://unsplash.com/photos/nGEhLj236V8)
+- [New York 4](https://unsplash.com/photos/Aa8_X_YgrO4)
+- [Japan 1](https://unsplash.com/photos/Dksk8szLRN0)
+- [Japan 2](https://unsplash.com/photos/vJqSAasmCEY)
+- [Japan 3](https://unsplash.com/photos/IjA4vTJ3PHY)
+- [Japan 4](https://unsplash.com/photos/NYyCqdBOKwc)
+
+Thanks to all photographers.
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const cms = require('cms');
+const consolidate = require('consolidate');
+const marked = require('marked');
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const cleanCss = require('gulp-clean-css');
+const minifyHtml = require('gulp-htmlmin');
+const runSequence = require('run-sequence');
+
+const src = path.resolve(__dirname, 'src');
+const content = path.resolve(src, 'content');
+const templates = path.resolve(src, 'templates');
+const output = path.resolve(__dirname, 'build');
+
+gulp.task('html', () => {
+  gulp.src(path.resolve(output, '**/*.html')).pipe(minifyHtml({
+    removeComments: true,
+    collapseWhitespace: true,
+    removeAttributeQuotes: true
+  })).pipe(gulp.dest(output));
+});
+
+gulp.task('css', () => {
+  gulp.src(path.resolve(src, 'scss', 'style.scss')).pipe(sass()).pipe(autoprefixer({
+    browsers: [
+      'last 2 versions'
+    ]
+  })).pipe(cleanCss()).pipe(gulp.dest(path.resolve(output, 'css')));
+});
+
+gulp.task('js', () => {
+  gulp.src(path.resolve(src, 'js', 'scripts.js')).pipe(babel()).pipe(uglify()).pipe(gulp.dest(path.resolve(output, 'js')));
+});
+
+gulp.task('cms', () => cms({
+  template: consolidate.pug,
+  paths: {
+    content: content,
+    templates: templates,
+    output: output
+  },
+  extensions: {
+    templates: [
+      'pug'
+    ]
+  },
+  shortcodes: {
+    youtube: (attrs, content) => {
+      return `<div class="youtube" style="padding-top:${(100 / attrs.width * attrs.height)}%">
+        <iframe src="https://www.youtube.com/embed/${attrs.id}" frameborder="0"></iframe>
+      </div>`;
+    }
+  },
+  addons: {
+    markdown: (input) => marked(input)
+  },
+  globals: {
+    site: 'CMS Demo'
+  }
+}));
+
+gulp.task('content', (done) => {
+  runSequence('cms', 'html', done);
+});
+
+gulp.task('build', [
+  'content',
+  'css',
+  'js'
+]);
+
+gulp.task('dev', [
+  'build'
+], () => {
+  gulp.watch([
+    path.resolve(content, '**/*'),
+    path.resolve(templates, '**/*'),
+    path.resolve(src, 'scss', '**/*'),
+    path.resolve(src, 'js', '**/*')
+  ], [
+    'build'
+  ]);
+});
